2172e37c626400263dafd86b779d666007b51d0d
YactayoC <syactayocrisostomo@gmail.com>
2021-10-21 21:40:48 -0500

    Cerrar sesion

--- Models/General_Interface.py
+++ Models/General_Interface.py
@@ -1,8 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel,QLineEdit,QPushButton,QMessageBox,QDialog
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QMessageBox, QDialog, QMainWindow
 from PyQt5.QtGui import QFont, QPixmap, QPalette, QBrush, QIcon
 from PyQt5.QtCore import Qt
 from Models import Login
+
 
 class General_Interface(QDialog):
     def __init__(self):
@@ -10,7 +11,7 @@ class General_Interface(QDialog):
         self.initialize()
 
     def initialize(self):
-        self.setGeometry(550,250,800,650)
+        self.setGeometry(550, 250, 800, 650)
         self.setWindowTitle("Registro Usuario")
         self.display_widgets()
         window_palette = QPalette()
@@ -18,16 +19,14 @@ class General_Interface(QDialog):
         self.setPalette(window_palette)
         self.display_widgets()
 
-
     def display_widgets(self):
-
+        # Labels
         self.lbl1 = QLabel(self)
         self.lbl1.setGeometry(550, 250, 500, 650)
         self.lbl1.move(300, 0)
         self.lbl1.setStyleSheet("border-radius: 0px;"
                                 "background-color: red;"
                                 "font-weight: bold; ")
-
 
         user_image = r"../Images/klipartz_opt.png"
         try:
@@ -45,41 +44,40 @@ class General_Interface(QDialog):
         self.user.move(30, 200)
         self.user.setStyleSheet("color: white;")
 
-        #btn
+        # Buttons
         self.btn_games = QPushButton("Juegos", self)
         self.btn_games.resize(200, 40)
         self.btn_games.move(50, 240)
         self.btn_games.clicked.connect(self.Action1)
         self.btn_games.setStyleSheet("border-radius: 10px;"
-                                "background-color: white;"
-                                "font-weight: bold; ")
+                                     "background-color: white;"
+                                     "font-weight: bold; ")
 
         self.btn_homework = QPushButton("Tarea", self)
         self.btn_homework.resize(200, 40)
         self.btn_homework.move(50, 300)
         self.btn_homework.clicked.connect(self.Action2)
         self.btn_homework.setStyleSheet("border-radius: 10px;"
-                                          "background-color: white;"
-                                          "font-weight: bold; ")
+                                        "background-color: white;"
+                                        "font-weight: bold; ")
 
         self.btn_read = QPushButton("Lecturas Recomendadas", self)
         self.btn_read.resize(200, 40)
         self.btn_read.move(50, 360)
         self.btn_read.clicked.connect(self.Action3)
         self.btn_read.setStyleSheet("border-radius: 10px;"
-                                          "background-color: white;"
-                                          "font-weight: bold; ")
+                                    "background-color: white;"
+                                    "font-weight: bold; ")
 
         self.btn_closesession = QPushButton("Cerrar Sesión", self)
         self.btn_closesession.resize(200, 40)
         self.btn_closesession.move(50, 420)
-        self.btn_closesession.clicked.connect(self.Action4)
+        self.btn_closesession.clicked.connect(self.sessionClose)
         self.btn_closesession.setStyleSheet("border-radius: 10px;"
-                                          "background-color: white;"
-                                          "font-weight: bold; ")
+                                            "background-color: white;"
+                                            "font-weight: bold; ")
 
     def Action1(self):
-
 
         print("Se ha cerrado la ventana")
 
@@ -87,14 +85,10 @@ class General_Interface(QDialog):
 
         print("Se ha cerrado la ventana")
 
-
     def Action3(self):
 
-
         print("Se ha cerrado la ventana")
 
-
-    def Action4(self):
+    def sessionClose(self):
         General_Interface.close(self)
-        print("Se ha cerrado la ventana")
-
+        Login.Login().exec_()
